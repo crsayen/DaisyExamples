6cbd5105955106c3a21b7b5f745aeaad0c398041
crsayen <crsayen@gmail.com>
2022-02-27 15:56:06 -0500

    working w/o FM

--- patch/SainchawFirmware/ChainSaw.cpp
+++ patch/SainchawFirmware/ChainSaw.cpp
@@ -2,22 +2,27 @@
 #include "sainchaw.h"
 #include <string>
 
-#define MAX_DETUNE 100
-#define SWARM_SIZE 1
+#define SWARM_SIZE 7
+#define SWRM_CTR_SHFT 3
 #define CENT 0.02f
 #define SEMITONE 0.166666667f
 #define BASE_FREQ 32.7f
+#define MAX_DETUNE 0.15f
+#define FM_HZ_PER_VOLT 200.0f
 
 using namespace daisy;
 using namespace daisysp;
 
 Sainchaw   sainchaw;
-Oscillator swarm_0[SWARM_SIZE];
+Oscillator swarm_0[SWARM_SIZE][2];
+Oscillator test_osc;
 
-int  detune_amt;
-bool alt_encoder_behavior;
-int  semitones;
-int  cents;
+float detune;
+bool  alt_encoder_behavior;
+int   semitones;
+int   cents;
+float osc_amplitude;
+float crossfade;
 
 void UpdateControls();
 
@@ -26,11 +31,11 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
                           size_t                    size) {
     UpdateControls();
     for(size_t i = 0; i < size; i++) {
-        //Process and output the three oscillators
         for(size_t chn = 0; chn < 2; chn++) {
             float sig = 0.f;
             for(int j = 0; j < SWARM_SIZE; j++) {
-                sig += swarm_0[j].Process();
+                sig += (1.0f - crossfade) * swarm_0[j][0].Process();
+                sig += crossfade * swarm_0[j][1].Process();
             }
             out[chn][i] = sig;
         }
@@ -39,9 +44,13 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
 
 void SetupOsc(float samplerate) {
     for(int i = 0; i < SWARM_SIZE; i++) {
-        swarm_0[i].Init(samplerate);
-        //swarm_0[i].SetAmp(0.14285);
-        swarm_0[i].SetAmp(1);
+        for(int j = 0; j < 2; j++) {
+            u_int8_t waveforms[2] = {Oscillator::WAVE_POLYBLEP_SAW,
+                                     Oscillator::WAVE_POLYBLEP_SQUARE};
+            swarm_0[i][j].Init(samplerate);
+            swarm_0[i][j].SetWaveform(waveforms[j]);
+            swarm_0[i][j].SetAmp(osc_amplitude);
+        }
     }
 }
 
@@ -50,10 +59,12 @@ int main(void) {
     sainchaw.Init(); // Initialize hardware (daisy seed, and patch)
     samplerate = sainchaw.AudioSampleRate();
 
-    detune_amt           = 0;
+    detune               = 0.f;
     alt_encoder_behavior = false;
     semitones            = 0;
     cents                = 0;
+    osc_amplitude        = .7f / SWARM_SIZE;
+    crossfade            = 0.0f;
 
     SetupOsc(samplerate);
     sainchaw.SetAltLed(false);
@@ -76,18 +87,36 @@ void handleEncoder() {
     }
 }
 
+
 void UpdateControls() {
     sainchaw.ProcessDigitalControls();
     sainchaw.ProcessAnalogControls();
 
-    // cv
+    // Detune and Shape cv
     float ctrl[Sainchaw::CTRL_LAST];
-    for(int i = 0; i < Sainchaw::CTRL_LAST; i++) {
-        ctrl[i] = sainchaw.GetKnobValue((Sainchaw::Ctrl)i);
+    for(int i = Sainchaw::DETUNE_CTRL; i < Sainchaw::SHAPE_CTRL + 1; i++) {
+        ctrl[i]
+            = sainchaw.GetKnobValue((Sainchaw::Ctrl)i) + 1.f * .5f; //normalized
     }
 
-    float pitch = ctrl[Sainchaw::PITCH_1_CTRL] * 5.f;           //voltage
-    pitch       = powf(2.f, ctrl[Sainchaw::PITCH_1_CTRL]) * 55; //Hz
+    // FM cv
+    float fm_val            = sainchaw.GetKnobValue(Sainchaw::FM_CTRL);
+    ctrl[Sainchaw::FM_CTRL] = fm_val * 8.f; //voltage
+
+    // Pitch cv
+    // for(int i = Sainchaw::PITCH_1_CTRL; i < Sainchaw::CTRL_LAST; i++) {
+    // float pitch_val = sainchaw.GetKnobValue((Sainchaw::Ctrl)i);
+    float pitch_val = sainchaw.GetKnobValue(Sainchaw::PITCH_1_CTRL);
+    ctrl[Sainchaw::PITCH_1_CTRL] = pitch_val * 5.f + 2; //voltage
+    // }
+
+    crossfade = DSY_CLAMP(ctrl[Sainchaw::SHAPE_CTRL], 0.f, 1.f);
+
+    detune = DSY_CLAMP(ctrl[Sainchaw::DETUNE_CTRL], 0.f, 1.f);
+
+    float fm = powf(2.f, ctrl[Sainchaw::FM_CTRL] * 6.875) * FM_HZ_PER_VOLT;
+
+    float pitch = powf(2.f, ctrl[Sainchaw::PITCH_1_CTRL]) * 32.7f; //Hz
 
 
     //encoder
@@ -95,20 +124,13 @@ void UpdateControls() {
 
     //Adjust oscillators based on inputs
     for(int i = 0; i < SWARM_SIZE; i++) {
-        swarm_0[i].SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+        // TODO: don't need to do all this stuff 7 times
+        float shift = (i - SWRM_CTR_SHFT) * detune * MAX_DETUNE;
+        shift += semitones + (cents * 0.12f);
+        float shifted_pitch = powf(2.0f, shift * kOneTwelfth) * pitch;
+        shifted_pitch       = DSY_CLAMP(shifted_pitch, 8.175f, 16742.4f);
 
-        if(semitones != 0) {
-            pitch = powf(2.0, (semitones / 12)) * pitch;
-        }
-
-        if(cents != 0) {
-            pitch = powf(2.0, (cents / 100)) * pitch;
-        }
-
-        if(pitch < 8.175f) {
-            pitch = 8.175f;
-        }
-
-        swarm_0[i].SetFreq(pitch);
+        swarm_0[i][0].SetFreq(shifted_pitch); // + fm);
+        swarm_0[i][1].SetFreq(shifted_pitch); // + fm);
     }
 }
--- patch/SainchawFirmware/scratch.js
+++ patch/SainchawFirmware/scratch.js
@@ -1,39 +1,70 @@
-function getFreq(input, semitones = 0, cents = 0) {
-  let pitch = input
-  const CENT = 0.02
-  const SEMITONE = 0.1666666666667
-  if (semitones > 0) {
-    pitch = pitch * semitones * SEMITONE
-  } else if (semitones < 0) {
-    pitch = -pitch / (semitones * SEMITONE)
-  }
+const PI_F = 3.1416
+const TWOPI_F = PI_F * 2
+const TWO_PI_RECIP = 1.0 / TWOPI_F
+let phase_ = 0
+let phase_n = 0
 
-  if (cents > 0) {
-    pitch = pitch * cents * CENT
-  } else if (cents < 0) {
-    pitch = -pitch / (cents * CENT)
-  }
-  console.log(
-    `inpt: ${input}\nsemi: ${semitones}\ncent: ${cents}\noutp: ${pitch}\n\n`
-  )
+function phase_inc(f) {
+  return TWOPI_F * f * (1 / 48000)
 }
 
-function f(p, s) {
-  const S = 0.1666666666666666667
-  if (s > 0) {
-    p = p * s * S
-  } else if (s < 0) {
-    p = (-1 / (s * S)) * p
+function polyblep(phase_inc, t) {
+  const dt = phase_inc * TWO_PI_RECIP
+  if (t < dt) {
+    t /= dt
+    return t + t - t * t - 1.0
+  } else if (t > 1.0 - dt) {
+    t = (t - 1.0) / dt
+    return t * t + t + t + 1.0
+  } else {
+    return 0.0
   }
-  return p
 }
 
-function getPitch(p, s) {
-  const divisor = Math.ceil(s / 12)
-  console.log(divisor)
-  const factor = 12 * divisor - s * divisor
-  console.log(factor)
-  return (p / divisor) * factor
+function process(f) {
+  let out
+  let t
+  let phaseinc = phase_inc(f)
+
+  // t = phase_ * TWO_PI_RECIP
+  // // console.log(t)
+  // if (phaseinc >= 0) {
+  //   out = 2.0 * t - 1.0
+  //   out -= polyblep(Math.abs(phaseinc), Math.abs(t))
+  // } else {
+  //   out = 2.0 * t + 1.0
+  //   out += polyblep(Math.abs(phaseinc), Math.abs(t))
+  // }
+
+  // out *= -1.0
+
+  // case WAVE_POLYBLEP_SQUARE:
+  t = phase_ * TWO_PI_RECIP
+  if (phaseinc > 0) {
+    out = phase_ < PI_F ? 1.0 : -1.0
+    out += polyblep(phaseinc, t)
+    out -= polyblep(phaseinc, (t + 0.5) % 1.0)
+  } else {
+    out = phase_ < -PI_F ? 1.0 : -1.0
+    out -= polyblep(-phaseinc, -t)
+    out += polyblep(-phaseinc, -(t - 0.5) % 1.0)
+  }
+  out *= 0.707 // ?
+
+  phase_ += phaseinc
+  if (phase_ > TWOPI_F) {
+    phase_ -= TWOPI_F
+  } else if (phase_ < -TWOPI_F) {
+    phase_ += TWOPI_F
+  }
+  return out
 }
 
-console.log(getPitch(36.7, -1))
+function r(n) {
+  return Math.round(n * 100) / 100
+}
+
+for (let i = 0; i < 40; i++) {
+  let out = process(-3000)
+  console.log(out)
+}
