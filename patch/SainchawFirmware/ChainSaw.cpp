d17fedfc55f797b1f524bf925129f8c7ee512ab1
unknown <crsayen@gmail.com>
2024-01-13 21:45:44 -0500

    wip

--- patch/SainchawFirmware/ChainSaw.cpp
+++ patch/SainchawFirmware/ChainSaw.cpp
@@ -2,32 +2,26 @@
 
 #include "Pitch.h"
 #include "configuration.h"
-#include "daisysp.h"
 #include "daisy_sainchaw.h"
+#include "daisysp.h"
 
 using namespace daisy;
 using namespace daisysp;
 
-const int   kMaxSwarmSize                    = 9;
-const float kBaseFrequency                   = 32.7f;
-const float kDetuneScale                     = 0.25f;
-const float kFmHzPerVolt                     = 100.0f;
-const float kNormalizationDetectionThreshold = -0.15f;
-const int   kNumSwarms                       = 3;
-const int   kProbeSequenceDuration           = 32;
-const int   kNumStereoChannels               = 2;
-const int   kConfigSlot                      = 0;
+const int   kMaxSwarmSize          = 9;
+const float kBaseFrequency         = 32.7f;
+const float kDetuneScale           = 0.25f;
+const float kFmHzPerVolt           = 100.0f;
+const int   kNumSwarms             = 3;
+const int   kProbeSequenceDuration = 32;
+const int   kNumStereoChannels     = 2;
+const int   kConfigSlot            = 0;
 
 enum EncState { SEMITONES, CENTS, SWARM_SIZE, LAST };
 
 Sainchaw                sainchaw;
 VariableShapeOscillator swarms[kNumSwarms][kMaxSwarmSize];
 
-// #define LOAD_METER
-
-#ifdef LOAD_METER
-CpuLoadMeter cpuLoadMeter;
-#endif
 
 CONFIGURATION current_config;
 bool          docal;
@@ -39,49 +33,16 @@ int           semitones_;
 int           cents_;
 float         lpfm_;
 bool          ignore_enc_switch_;
-int           normalization_detection_count_;
-int           normalization_detection_mismatches_[kNumSwarms];
-uint32_t      normalization_probe_state_;
 bool          voct_patched_[kNumSwarms];
 int           waitcount_;
-uid_t         cpuLoad_2_bits;
 Pitch         pitch[kNumSwarms];
 
 void UpdateControls();
-void DetectNormalization();
 void handleCalibration();
 bool cal_handleEncoder();
 void cal_read_voct();
 void save_config(uint32_t slot);
 void load_config(uint32_t slot);
-
-
-void blockStart() {
-#ifdef LOAD_METER
-  cpuLoadMeter.OnBlockStart();
-#endif
-}
-
-void blockEnd() {
-#ifdef LOAD_METER
-  cpuLoadMeter.OnBlockEnd();
-  float load = cpuLoadMeter.GetAvgCpuLoad();
-  if(load > .75f) {
-    cpuLoad_2_bits = 0b11;
-  } else if(load > .5f) {
-    cpuLoad_2_bits = 0b10;
-  } else if(load > .25f) {
-    cpuLoad_2_bits = 0b01;
-  } else {
-    cpuLoad_2_bits = 0b00;
-  }
-#endif
-}
-
-void displayLoadMeter() {
-  sainchaw.SetAltLed(cpuLoad_2_bits & 0x01);
-  sainchaw.SetNoteLed(cpuLoad_2_bits & 0x02);
-}
 
 static void
 AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size) {
@@ -119,25 +80,20 @@ int main(void) {
   samplerate = sainchaw.AudioSampleRate();
   load_config(kConfigSlot);
 
-  encoderState_                  = EncState::SEMITONES;
-  docal                          = false;
-  swarmSize_                     = 5;
-  swarmCenterShift_              = 2;
-  amplitudeReduction_            = .12f / swarmSize_;
-  semitones_                     = 0;
-  cents_                         = 0;
-  ignore_enc_switch_             = false;
-  normalization_detection_count_ = 0;
-  normalization_probe_state_     = 123456789;
-  waitcount_                     = 0;
-
-#ifdef LOAD_METER
-  cpuLoadMeter.Init(samplerate, sainchaw.AudioBlockSize());
-#endif
+  encoderState_       = EncState::SEMITONES;
+  docal               = false;
+  swarmSize_          = 5;
+  swarmCenterShift_   = 2;
+  amplitudeReduction_ = .12f / swarmSize_;
+  semitones_          = 0;
+  cents_              = 0;
+  ignore_enc_switch_  = false;
+  waitcount_          = 0;
 
   for(int i = 0; i < kNumSwarms; i++) {
-    voct_patched_[i]                       = false;
-    normalization_detection_mismatches_[i] = 0;
+    voct_patched_[i] = false;
+    // TODO: make a UI to turn inputs on and off
+    if(i == 0) { voct_patched_[i] = true; }
     pitch[i].SetByNote(0);
   }
 
@@ -195,45 +151,10 @@ void handleEncoder() {
                      || encoderState_ == EncState::SWARM_SIZE);
 }
 
-void DetectNormalization() {
-  // apparently either my ADCs or my GPIO are too slow to
-  // reliably perform normalization detection at 1khz+.
-  // so we wait for 5 update cycles (approx 5ms) to check
-  // for the normalization noise
-  if(waitcount_++ < 5) { return; }
-  waitcount_ = 0;
-
-  bool expected_state = normalization_probe_state_ >> 31;
-
-  for(int i = 0; i < kNumSwarms; i++) {
-    float actual_adc_value = sainchaw.GetKnobValue(
-        (Sainchaw::Ctrl)(i + Sainchaw::PITCH_1_CTRL)); // approx ( -.2 | -.06 )
-
-    bool read_state = actual_adc_value > kNormalizationDetectionThreshold;
-    if(read_state != expected_state) { normalization_detection_mismatches_[i]++; }
-  }
-
-  ++normalization_detection_count_;
-  if(normalization_detection_count_ >= kProbeSequenceDuration) {
-    normalization_detection_count_ = 0;
-    for(int i = 0; i < kNumSwarms; ++i) {
-      voct_patched_[i] = normalization_detection_mismatches_[i] >= 4;
-      normalization_detection_mismatches_[i] = 0;
-    }
-  }
-
-  normalization_probe_state_ = 1103515245 * normalization_probe_state_ + 12345;
-  sainchaw.SetNormalizationProbe(normalization_probe_state_ >> 31);
-}
-
 void UpdateControls() {
   sainchaw.ProcessDigitalControls();
   sainchaw.ProcessAnalogControls();
-  DetectNormalization();
 
-#ifdef LOAD_METER
-  displayLoadMeter();
-#endif
 
   float shape_amt = DSY_CLAMP(
       ((sainchaw.GetKnobValue(Sainchaw::SHAPE_CTRL) + 1) * .5), .0f, 1.f); // 0.0 to 1.0
@@ -371,11 +292,11 @@ void save_config(uint32_t slot) {
   uint32_t base = 0x90000000;
   base += slot * 4096;
   sainchaw.seed.qspi.Erase(base, base + sizeof(CONFIGURATION));
-  sainchaw.seed.qspi.Write(base, sizeof(CONFIGURATION), (uint8_t*)&current_config);
+  sainchaw.seed.qspi.Write(base, sizeof(CONFIGURATION), (uint8_t *)&current_config);
 }
 
 void load_config(uint32_t slot) {
   memcpy(&current_config,
-         reinterpret_cast<void*>(0x90000000 + (slot * 4096)),
+         reinterpret_cast<void *>(0x90000000 + (slot * 4096)),
          sizeof(CONFIGURATION));
 }
