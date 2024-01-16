d18d364cdeaa0e89f46e4bf2d4592a43619c9826
crsayen <crsayen@gmail.com>
2024-01-15 20:12:15 -0500

    wip

--- patch/SainchawFirmware/toying.py
+++ patch/SainchawFirmware/toying.py
@@ -1,34 +1,9 @@
-import time
+import math
+import matplotlib.pyplot as plt
 TWO_PI_RECIP = 0.15915494
-TWOPI_F = 2.0 * 3.1415927410125732421875
+PI_F = 3.1415927410125732421875
+TWOPI_F = 2.0 * PI_F
 SQUARE_FUDGE = 0.83333
-
-def calculate_tri(phase_, pw_):
-    saw = 0;
-    square = 0;
-    t = phase_ * TWO_PI_RECIP;
-    t_scaled_to_pw = (t + 1.0) / 2.0
-
-    if pw_ < 0.5:
-        if t_scaled_to_pw < pw_:
-            square = pw_ * SQUARE_FUDGE
-            saw =   (-1 * t_scaled_to_pw) + pw_
-        else:
-            square = 0
-            saw = t_scaled_to_pw - pw_
-    else:
-        if phase_ < 0.0:
-            square = pw_ * SQUARE_FUDGE
-            saw =  -0.5 * t
-        else:
-            square = 0
-            saw = 0.5 * t
-        saw *= 2 - 2 * pw_
-    return 2 * (square + saw) - 1, t, t_scaled_to_pw
-                                
-# print(calculate(0))
-# print(calculate(0.25))
-
 
 def polyblep(phase_inc, t):
   dt = phase_inc * TWO_PI_RECIP
@@ -41,23 +16,82 @@ def polyblep(phase_inc, t):
   else:
     return 0.0
 
-resolution = 25
+def calc_phase_inc(f): return (TWOPI_F * f) * sr_recip_
 
-def do_cycle(pw):
-    for i in range(resolution):
-        p = ((i / (resolution / 2)) - 1) * TWOPI_F
-        val, t, t_s = calculate_tri(p, pw)
-        # val = calculate_tri(p, pw)
-        line = f'.{" " * (resolution)}.{" " * (resolution)}.'
-        idx = int(((val * 0.5) + 0.5) * (2 * resolution))
-        idx = idx if 0 < idx < (resolution * 2) else None
-        stats = f'v:{str(round(val, 3))[0:4]}, t:{str(round(t, 3))[0:4]}, ts:{str(round(t_s, 3))[0:4]}'
-        line_list = [*line]
-        if idx is not None: line_list[idx] = 'x'
-        line = ''.join(line_list)
-        print(f"{line}  {stats}")
+sr_        = 48000
+sr_recip_  = 1.0 / sr_
+freq_      = 100.0
+phase_inc = calc_phase_inc(freq_)
+phase_ = 0
 
-for percent in range(0, 100, 5):
-    pw = percent / 100
-    do_cycle(pw)
-    time.sleep(0.05)
+
+def calculate_tri(pw_):
+    global phase_
+    saw = 0;
+    square = 0;
+    pw_scaled_0_2pi = pw_ * TWOPI_F
+    pw_scaled_m1_1 = pw_ * 2 - 1
+    phase_scaled_m1_1 = phase_ * TWO_PI_RECIP * 2 - 1
+    t = phase_scaled_m1_1 * 0.5 + 0.5
+    
+    pw_lt_half = pw_ < 0.5
+    phase_lt_half = phase_ < PI_F
+
+    if pw_lt_half:
+        if phase_ < pw_scaled_0_2pi:
+            square = (pw_scaled_m1_1 * 2 + 1)
+            saw = phase_scaled_m1_1
+        else:
+            square = -1
+            saw = -phase_scaled_m1_1
+        square += polyblep(phase_inc, t);
+        square -= polyblep(phase_inc, math.fmod(t + (1 - pw_), 1.0));
+        saw += polyblep(phase_inc, t);
+        saw -= polyblep(phase_inc, math.fmod(t + (1 - pw_), 1.0));
+    else:
+        if phase_lt_half:
+            square = 1
+            saw = phase_scaled_m1_1
+        else:
+            square = -1
+            saw = -phase_scaled_m1_1
+        square += polyblep(phase_inc, t);
+        square -= polyblep(phase_inc, math.fmod(t + 0.5, 1.0));
+        saw += polyblep(phase_inc, t);
+        saw -= polyblep(phase_inc, math.fmod(t + 0.5, 1.0));
+        saw *= 2 - 2 * pw_
+        
+    phase_ += phase_inc;
+    if phase_ > TWOPI_F:
+        phase_ -= TWOPI_F
+
+    return saw, square, (saw + square) * 0.45
+
+
+
+
+def print_plot():
+    pw = 0.25
+    n_cycles = 5
+    sweep_pw = False
+    plot_spread = 0
+    # sweep_pw = True
+
+    _n_samples = 48000 // (100 // n_cycles)
+    _saw_samples = []
+    _square_samples = []
+    _osc_samples = []
+    for i in range(_n_samples):
+        if sweep_pw:
+            pw = i / _n_samples
+        saw, square, osc = calculate_tri(pw)
+        _saw_samples.append(saw - plot_spread)
+        _square_samples.append(square + plot_spread)
+        _osc_samples.append(osc)
+    plt.plot(_saw_samples)
+    plt.plot(_square_samples)
+    plt.plot(_osc_samples)
+    plt.show()
+    
+print_plot()
+
